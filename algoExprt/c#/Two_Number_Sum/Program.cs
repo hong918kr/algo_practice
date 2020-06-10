66a607fc5e6188a8bf67cc1bf5d13d6dfe6f936f
Don Hong <hong918kr@gmail.com>
2020-06-10 00:48:36 -0700

    [Add] c# directory

---
+++ algoExprt/c#/Two_Number_Sum/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Two_Number_Sum
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
