31d0cb2854cb688cb3a4c6406c0aa43a1ba83425
hong918kr <hong918kr@gmail.com>
2020-06-11 05:32:58 -0700

    validate_subsequence

---
+++ algoExprt/c#/Validate_Subsequence/Validate_Subsequence/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Two_Number_Sum
+{
+    public class Solution
+    {
+        public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+        {
+
+
+            return false;
+        }
+
+    }
+}
