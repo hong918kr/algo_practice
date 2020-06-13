61b1dedf114dce7de684586f3589b4deebbc10d0
hong918kr <hong918kr@gmail.com>
2020-06-12 21:51:43 -0700

    [Add] website solution

--- algoExprt/c#/Validate_Subsequence/Validate_Subsequence/Solution.cs
+++ algoExprt/c#/Validate_Subsequence/Validate_Subsequence/Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Two_Number_Sum
@@ -44,5 +45,41 @@ namespace Two_Number_Sum
 
         }
 
+        public static bool IsValidSubsequence_sol_1(List<int> array, List<int> sequence)
+        {
+            int arrIdx = 0;
+            int seqIdx = 0;
+            while (arrIdx < array.Count && seqIdx < sequence.Count)
+            {
+                if (array[arrIdx] == sequence[seqIdx])
+                {
+                    seqIdx++;
+                }
+                arrIdx++;
+            }
+            return seqIdx == sequence.Count;
+
+
+        }
+
+        public static bool IsValidSubsequence_sol_2(List<int> array, List<int> sequence)
+        {
+            int seqIdx = 0;
+            foreach (var val in array)
+            {
+                if (seqIdx == sequence.Count)
+                {
+                    break;
+                }
+                if (sequence[seqIdx] == val)
+                {
+                    seqIdx++;
+                }
+            }
+            return seqIdx == sequence.Count;
+
+
+
+        }
     }
 }
