c95553bd9ca31173272a09224394ee532e937878
hong918kr <hong918kr@gmail.com>
2020-06-16 18:44:16 -0700

    update questions

---
+++ algoExprt/c#/SmallestDifference/SmallestDifference/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SmallestDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Start");
+            ProgramTest.TestCase1();
+            ProgramTest.TestCase2();
+            Console.WriteLine("Start");
+        }
+
+        public static int[] SmallestDifference(int[] arrayOne, int[] arrayTwo)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ algoExprt/c#/SmallestDifference/SmallestDifference/ProgramTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SmallestDifference
+{
+    public class ProgramTest
+    {
+
+        public static void TestCase1()
+        {
+            int[] inputOne_1 = new int[] { -1, 5, 10, 20, 28, 3 };
+            int[] inputTwo_1 = new int[] { 26, 134, 135, 15, 17 };
+            int[] output_1 = new int[] { 28, 26 };
+           
+            Debug.Assert(Program.SmallestDifference(inputOne_1, inputTwo_1) == output_1);
+            Console.WriteLine("TestCase1 Pass");
+        }
+
+        public static void TestCase2()
+        {
+            Console.WriteLine("TestCase2");
+        }
+
+
+
+    }
+}
