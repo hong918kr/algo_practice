8625cbc6fa2a81c46b850db5c101dc4160cb36f7
hong918kr <hong918kr@gmail.com>
2020-06-17 22:42:35 -0700

    set up the MoveElementToEnd

---
+++ algoExprt/c#/MoveElementToEnd/MoveElementToEnd/MoveElementToEnd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoveElementToEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Start");
+            ProgramTest.TestCase1();
+            Console.WriteLine("Hello World!");
+        }
+
+        public static List<int> MoveElementToEnd(List<int> array, int toMove)
+        {
+            return null;
+        }
+    }
+}
---
+++ algoExprt/c#/MoveElementToEnd/MoveElementToEnd/MoveElementToEnd/ProgramTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace MoveElementToEnd
+{
+    class ProgramTest
+    {
+        public static void TestCase1()
+        {
+            List<int> array1 = new List<int>()
+            {
+                2, 1, 2, 2, 2, 3, 4, 2
+            };
+            int toMove1 = 2;
+            List<int> expectedStart1 = new List<int>()
+            {
+                1, 3, 4
+            };
+            List<int> expectedEnd1 = new List<int>()
+            {
+                2, 2, 2, 2, 2
+            };
+            List<int> output = Program.MoveElementToEnd(array1, toMove1);
+            List<int> outputStart = output.GetRange(0, 3);
+            outputStart.Sort();
+            List<int> outputEnd = output.GetRange(3, output.Count - 3);
+            
+
+            return;
+        }
+
+        public static void TestCase2()
+        {
+            return;
+        }
+
+    }
+}
